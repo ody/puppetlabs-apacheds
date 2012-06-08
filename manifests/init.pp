3121eaa92eb8f55438b39125b862ed1b5d5e524f
Cody Herriges <c.a.herriges@gmail.com>
2012-06-08 16:59:19 -0700

    WIP

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,9 @@ class apacheds(
   $port        = '10389',
   $ssl_port    = '10636',
   $parition_dn = hiera('apacheds::partition_dn'),
-  $jks_pw      = hiera('apacheds::jks_pw'),
+  $jks         = '',
+  $jks_pw      = '',
+  $replica_id  = '1',
   $version,
   $master
 ) {
@@ -12,49 +14,10 @@ class apacheds(
 
   package { 'apacheds':
     ensure  => $version,
-    before  => File['/etc/apacheds'],
     require => [ Class['java'], Class['apacheds::config'] ],
   }
 
   File { mode => '0644', owner => 'apacheds', group => 'root', }
-
-  file { '/etc/apacheds':
-    ensure => directory,
-  }
-
-  file { '/etc/apacheds/certs':
-    ensure => directory,
-    mode   => '0750',
-    before => Java_ks[$::fqdn],
-  }
-
-  file { "/etc/apacheds/certs/${::fqdn}.pem":
-    source => "puppet:///modules/ssldata/${fqdn}_ldap.pem",
-  }
-
-  file { "/etc/apacheds/certs/${::fqdn}.key":
-    source => "puppet:///modules/ssldata/${fqdn}_ldap.key",
-  }
-
-  file { '/etc/apacheds/certs/ca.pem':
-    source => 'puppet:///modules/ssldata/ca.pem',
-  }
-
-  java_ks { $::fqdn:
-    ensure      => latest,
-    password    => $jks_pw,
-    certificate => "/etc/apacheds/certs/${::fqdn}.pem",
-    pirvate_key => "/etc/apacheds/certs/${::fqdn}.key",
-    target      => "/var/lib/apacheds-${version}/default/apacheds.jks",
-  }
-
-  java_ks { 'ca':
-    ensure       => latest,
-    password     => $jks_pw,
-    certificate  => '/etc/apacheds/certs/ca.pem',
-    target       => "/var/lib/apacheds-${version}/default/apacheds.jks",
-    trustcacerts => true,
-  }
 
   if $master {
 
